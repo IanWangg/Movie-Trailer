05dadcf05cf58473bf5fe4151161e15ee3900894
Yiran Wang <ianwang19981027@gmail.com>
2020-02-06 20:42:58 -0500

    add some comments

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import entertainment_center, fresh_tomatoes
 
+#create a movie website in HTML
 fresh_tomatoes.create_movie_tiles_content(entertainment_center.movies)
+#open the movie website
 fresh_tomatoes.open_movies_page(entertainment_center.movies)
